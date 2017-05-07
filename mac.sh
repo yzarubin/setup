8a986c3a0b19bbd367309e4f358c3a335c073d67
Yuri Zarubin <yuri@yza.io>
2017-05-06 21:47:32 -0700

    Update mac.sh

--- mac.sh
+++ mac.sh
@@ -1,9 +1,6 @@
 
 sudo xcodebuild -license
 
-# Xcode command line tools
-xcode-select --install;
-
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
 brew install caskroom/cask/brew-cask;
 brew tap caskroom/versions;
@@ -12,7 +9,6 @@ brew tap caskroom/versions;
 brew install gcc --force-bottle
 brew cask install java;
 brew cask install iterm2;
-brew install git;
 brew install wget;
 brew cask install sublime-text3;
 
