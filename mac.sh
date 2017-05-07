c7b64c6a27a8c9271f9b3ff0daac234250b6b109
Yuri Zarubin <yuri@yza.io>
2017-05-06 22:24:29 -0700

    Update mac.sh

--- mac.sh
+++ mac.sh
@@ -1,4 +1,7 @@
 
+cd ~/;
+touch .bash_profile;
+
 sudo xcodebuild -license
 
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
@@ -10,19 +13,22 @@ brew install gcc --force-bottle
 brew cask install java;
 brew cask install iterm2;
 brew install wget;
-brew cask install sublime-text3;
+brew cask install sublime-text;
 
 # git
 git config --global user.name "Yuri Zarubin"
 git config --global user.email "yuri@yza.io"
 git config --global github.user yzarubin
-git config --global core.editor "subl3 -w"
+git config --global core.editor "subl -w"
 
 # rust tool chain
 curl https://sh.rustup.rs -sSf | sh
 
+
+
 # nodejs
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+source ~/.bash_profile
 nvm install node
 
 # apps
